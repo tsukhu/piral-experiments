9d05925c9ad18a9cc52ae1482c8194004d5b6577
Tarun Kumar Sukhu - ERS <tsukhu@hcl.com>
2020-01-29 17:15:16 +0530

    base version

--- my-litelem-pilet/src/index.ts
+++ my-litelem-pilet/src/index.ts
@@ -1,21 +1,25 @@
 import { PiletApi } from "my-app";
 import "./styles/index.scss";
 import "./styles/tailwind.css";
-import { createTile } from "./tile";
 import { createMainPage } from "./main-page";
-import { createMenu } from "./my-menu";
+
 
 /**
  * Shows an API extension using LitElement components.
  */
 export function setup(piral: PiletApi) {
-  const tile = createTile();
   const mypage = createMainPage();
-  const mymenu = createMenu();
- // piral.registerMenu(piral.fromLitEl(mymenu));
   piral.registerPage("/lit-page", piral.fromLitEl(mypage));
-/*   piral.registerTile(piral.fromLitEl(tile), {
-    initialColumns: 2,
-    initialRows: 2
-  }); */
+  piral.registerMenu(() => piral.createLink("/lit-page", "WebComponent Page"));
+  piral.registerTile(
+    () => piral.createTile(
+      "/lit-page",
+      "My LitElem Pilet",
+      "say hello to LitElem from extension api"
+    ),
+    {
+      initialColumns: 2,
+      initialRows: 2
+    }
+  );
 }
